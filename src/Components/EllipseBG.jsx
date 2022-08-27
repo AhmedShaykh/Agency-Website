b2b57c309d736549a86845ca2ef5e4c669b41f15
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-27 17:43:15 +0500

    Git Init

---
+++ src/Components/EllipseBG.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function EllipseBG() {
+    return (
+        <div className="ellipse-bg"></div>
+    )
+}
+
+export default EllipseBG;
--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import MainText from "./MainText";
-import Navbar from "./Navbar";
 import SideTitle from "./SideTitle";
+import Navbar from "./Navbar";
+import EllipseBG from "./EllipseBG";
 import Shoe from './Shoe.jsx';
+import MainText from "./MainText";
 
 function MainPage() {
     return (
         <div className="main">
             <SideTitle />
             <Navbar />
+            <EllipseBG />
             <Shoe />
             <MainText />
         </div>
