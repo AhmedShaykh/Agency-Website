ee90ba131d90af2da9091cb9e022c4812d237ba1
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-27 17:34:16 +0500

    Git Init

---
+++ src/Components/Shadow.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Shadow() {
+    return (
+        <div className="shadow"></div>
+    )
+}
+
+export default Shadow;
--- src/Components/Shoe.jsx
+++ src/Components/Shoe.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import shoe from '../Assets/Shoe.png';
+import Shadow from "./Shadow";
 
 function Shoe() {
     return (
         <div className="shoe">
             <img src={shoe} alt="/" />
+            <Shadow />
         </div>
     )
 }
