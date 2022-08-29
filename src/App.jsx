126dbc10609d411249154b8acf608e64fa6a3974
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-08-29 19:09:51 +0500

    Git Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 import './App.css';
-import SideTitle from './Components/SideTitle';
-import Navbar from './Components/Navbar';
-import EllipseBG from './Components/EllipseBG';
-import Shoe from './Components/Shoe';
-import MainText from './Components/MainText';
-import Ellipse from './Components/Ellipse';
 
 function App() {
   return (
-    <div className='main'>
-      <SideTitle />
-      <Navbar />
-      <EllipseBG />
-      <Shoe />
-      <MainText />
-      <Ellipse />
-    </div>
+    <div></div>
   );
 }
 
--- src/Components/EllipseBG.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function EllipseBG() {
-    return (
-        <div className="ellipse-bg"></div>
-    )
-}
-
-export default EllipseBG;
