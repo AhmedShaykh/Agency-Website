9749ba3ca5a06a3dfa155d41bf70dbf98aa948f6
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-31 22:56:23 +0500

    Git Init

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,35 @@
 import React, { useState } from 'react';
-import { SiDatabricks } from 'react-icons/si';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { FaServer, FaBars, FaTimes, } from 'react-icons/fa';
+// import { Link } from 'react-router-dom';
 import './NavbarStyles.css';
 
 const Navbar = () => {
 
-    const [nav, setNav] = useState(false);
-    const handleNav = () => setNav(!nav);
+    // const [nav, setNav] = useState(false);
+    // const handleNav = () => setNav(!nav);
 
     return (
         <div name='top' className='navbar'>
             <div className="container">
                 <div className="logo">
-                    <SiDatabricks className='icon' />
+                    <FaServer className='icon' />
                     <h3 style={{ fontSize: "2.3rem", paddingLeft: "0.5rem" }}>DATA CLOUD</h3>
                 </div>
 
-                {/* <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
-                    <li><Link to='/'>Home</Link></li>
+                <ul className='nav-menu'>
+                    {/* <li><Link to='/'>Home</Link></li>
                     <li><Link to='/recovery'>Recovery</Link> </li>
                     <li><Link to='/cloud'>Cloud</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
-                </ul> */}
+                    <li><Link to='/contact'>Contact</Link></li> */}
+                    <li>Home</li>
+                    <li>Recovery</li>
+                    <li>Cloud</li>
+                    <li>Contact</li>
+                </ul>
 
-                {/* <div className="hamburger" onClick={handleNav}>
+                <div className="hamburger" onClick={handleNav}>
                     {!nav ? (<FaBars className='icon' />) : (<FaTimes className='icon' />)}
-                </div> */}
+                </div>
             </div>
         </div>
     )
