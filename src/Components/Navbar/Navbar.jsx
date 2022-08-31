ea70bd63677bfc779565aaa873abebe9e3524cbd
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-01 00:18:31 +0500

    Git Init

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,26 +5,26 @@ import './NavbarStyles.css';
 
 const Navbar = () => {
 
-    // const [nav, setNav] = useState(false);
-    // const handleNav = () => setNav(!nav);
+    const [nav, setNav] = useState(false);
+    const handleNav = () => setNav(!nav);
 
     return (
         <div name='top' className='navbar'>
             <div className="container">
                 <div className="logo">
                     <FaServer className='icon' />
-                    <h3 style={{ fontSize: "2.3rem", paddingLeft: "0.5rem" }}>DATA CLOUD</h3>
+                    <h3 className='logo-text'>CYBER CITY</h3>
                 </div>
 
-                <ul className='nav-menu'>
+                <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
                     {/* <li><Link to='/'>Home</Link></li>
                     <li><Link to='/recovery'>Recovery</Link> </li>
                     <li><Link to='/cloud'>Cloud</Link></li>
                     <li><Link to='/contact'>Contact</Link></li> */}
-                    <li>Home</li>
-                    <li>Recovery</li>
-                    <li>Cloud</li>
-                    <li>Contact</li>
+                    <li className='li'>Home</li>
+                    <li className='li'>Recovery</li>
+                    <li className='li'>Cloud</li>
+                    <li className='li'>Contact</li>
                 </ul>
 
                 <div className="hamburger" onClick={handleNav}>
