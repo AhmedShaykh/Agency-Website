209de1c6a22adcd7dde7b0a4059223d387b4aa07
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-31 18:55:53 +0500

    Git Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Navbar from './Components/Navbar/Navbar.jsx';
+// import Hero from './Components/Hero/Hero.jsx';
+// import Data from './Components/Data/Data.jsx';
+// import Cloud from './Components/Cloud/Cloud.jsx'
+// import Footer from './Components/Footer/Footer.jsx';
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
     </div>
+    <>
+      <Navbar />
+      {/* <Hero /> */}
+      {/* <Data /> */}
+      {/* <Cloud /> */}
+      {/* <Footer /> */}
+    </>
   );
 }
 
