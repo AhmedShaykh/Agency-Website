62fa8d9f068f8d4e72cf65d4a745c6e706610da8
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-31 19:28:02 +0500

    Git Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Navbar from './Components/Navbar/Navbar.jsx';
-// import Hero from './Components/Hero/Hero.jsx';
+import Hero from './Components/Hero/Hero.jsx';
 // import Data from './Components/Data/Data.jsx';
 // import Cloud from './Components/Cloud/Cloud.jsx'
 // import Footer from './Components/Footer/Footer.jsx';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
     <>
       <Navbar />
-      {/* <Hero /> */}
+      <Hero />
       {/* <Data /> */}
       {/* <Cloud /> */}
       {/* <Footer /> */}
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './HeroStyles.css';
 
 const Hero = () => {
     return (
-        <div>
-
+        <div className='hero'>
+            <div className="container">
+                <div className="content">
+                    <h1>Data</h1>
+                    <h1 className='blue'>Secured</h1>
+                    <h1>Protection</h1>
+                    <div><button>Learn More</button></div>
+                </div>
+            </div>
         </div>
     )
 }
