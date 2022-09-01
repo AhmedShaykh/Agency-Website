0c5915309aca181d5d5275b5bb4523ef2ccd6b2b
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-01 19:03:14 +0500

    Git Init

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactStyles.css';
 
 const Contact = () => {
+
+    const [userData, setUserData] = useState({});
     return (
         <div className='contact'>
             <div className="container">
@@ -10,15 +12,15 @@ const Contact = () => {
                         <h1><span>Contact</span> Us</h1>
                         <div>
                             <label>Name</label>
-                            <input type="text" placeholder='Enter your name' />
+                            <input type="text" placeholder='Enter your Full Name' />
                         </div>
                         <div>
                             <label>Email</label>
-                            <input type="email" placeholder='Enter your email' />
+                            <input type="email" placeholder='Enter your Email' />
                         </div>
                         <div>
                             <label>Message</label>
-                            <textarea rows='10' placeholder='Enter your name' />
+                            <textarea rows='10' placeholder='Enter your Message' />
                         </div>
                         <button>Submit</button>
                     </form>
