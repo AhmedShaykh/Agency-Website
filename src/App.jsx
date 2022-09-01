3539a2340afc1626febc2564d4ecf49e34d710a5
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-01 16:48:30 +0500

    Git Init

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from './Components/Navbar/Navbar.jsx';
 import Hero from './Components/Hero/Hero.jsx';
 import Data from './Components/Data/Data.jsx';
+import Recovery from "./Components/Recovery/Recovery.jsx";
 import Cloud from './Components/Cloud/Cloud.jsx'
 import Contact from "./Components/Contact/Contact.jsx";
 import Footer from './Components/Footer/Footer.jsx';
@@ -11,6 +12,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Recovery />
       <Data />
       <Cloud />
       <Contact />
---
+++ src/Routes/ContactPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Contact from '../Components/Contact/Contact';
+import Footer from '../Components/Footer/Footer';
+import Navbar from '../Components/Navbar/Navbar';
+
+const ContactPage = () => {
+    return (
+        <>
+            <Navbar />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default ContactPage;
